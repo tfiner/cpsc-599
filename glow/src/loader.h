e71071846b10e069e6347751dd7d1ba832eb2bef
Tim Finer <tfiner@fastmail.fm>
2014-08-16 23:51:52 -0700

    Moved loading into the library.  The executable is tiny now!

---
+++ glow/src/loader.h
@@ -0,0 +1,15 @@
+// Tim Finer 
+// tfiner@csu.fullerton.edu
+// CPSC-599
+
+#pragma once
+
+
+namespace glow {
+
+
+    // Runs just like this is the command line for main.
+    int run(int argc, char** argv);
+
+
+} // glow
