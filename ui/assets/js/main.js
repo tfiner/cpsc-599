dc99050dad6b64153507cb29c87c6f4c20cb2c47
Tim Finer <tfiner@fastmail.fm>
2014-09-16 22:55:36 -0700

    Simpler layout, but still not happy with it...

--- ui/assets/js/main.js
+++ ui/assets/js/main.js
@@ -9,44 +9,18 @@ require.config({
         // Also, the path should NOT include
         // the '.js' file extension.
         jquery: 'jquery-1.11.1.min',
-        jquery_ui: 'jquery-ui.min'
+        jquery_ui: 'jquery-ui.min',
+        jquery_layout: 'jquery.layout-latest'
     }
 });
 
-// require(['jquery'], function( $ ) {
-//     console.log( $ ) // OK
-// });
-
-
-// Start the main app logic.
-requirejs(['jquery', 'jquery_ui'],
+requirejs(['jquery', 'jquery_ui', 'jquery_layout'],
 function($) {
-    var pane1 = $(".resizable1");
-
     console.log("All loaded.");
-    console.log(pane1);
-
-    pane1.resizable(
-    {
-        autoHide: true,
-        handles: 'e',
-        resize: function(e, ui) 
-        {
-            var parent = ui.element.parent();
-            var remainingSpace = parent.width() - ui.element.outerWidth(),
-                divTwo = ui.element.next(),
-                divTwoWidth = (remainingSpace - (divTwo.outerWidth() - divTwo.width()))/parent.width()*100+"%";
-                divTwo.width(divTwoWidth);
-        },
-        stop: function(e, ui) 
-        {
-            var parent = ui.element.parent();
-            ui.element.css(
-            {
-                width: ui.element.width()/parent.width()*100+"%",
-            });
-        }
+    $('body').layout({ 
+        applyDefaultStyles: true,
+        west__paneSelector: "#west",
+        east__paneSelector: "#east"         
     });
-
 });
 
