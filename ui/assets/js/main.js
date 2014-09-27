9a76f7b9e8afbc70df96266190833fd2b6e615b4
Tim Finer <tfiner@fastmail.fm>
2014-09-27 13:43:36 -0700

    Split intialization of user interface into pieces.  I stumbled upon the fix for the network!

--- ui/assets/js/main.js
+++ ui/assets/js/main.js
@@ -17,12 +17,13 @@ require.config({
 });
 
 requirejs(
-    ['jquery', 'jquery_ui', 'dockspawn', 'jsoneditor', 'cytoscape'],
+    ['jquery', 'dockspawn'],
 
-    function($, _, ds, JSONEditor) {
-        console.log("All loaded.");
+    function($, _, ds) {
+        console.log("Dockspawn loaded...");
 
-        // Turn on tooltips, ugh these are annoying.
+        // Turn on tooltips, ugh these are annoying 
+        // (don't forget to include jquery_ui if using this again...)
         // $( document ).tooltip();
 
         // Convert a div to the dock manager.  Panels can then be docked on to it
@@ -40,58 +41,33 @@ requirejs(
         window.onresize = onResized;
         onResized(null);
 
-        // Convert existing elements on the page into "Panels".
-        // They can then be docked on to the dock manager
-        // Panels get a titlebar and a close button, and can also be
-        // converted to a floating dialog box which can be dragged / resized
+        var status = new dockspawn.PanelContainer($("#status_window")[0], dockManager),
 
-        var status = new dockspawn.PanelContainer($("#status_window")[0], dockManager);
+            editorDiv = $("#editor1_window")[0],
+            editor1 = new dockspawn.PanelContainer(editorDiv, dockManager),
+            noiseDiv = $("#noise_window"),
+            noise = new dockspawn.PanelContainer(noiseDiv[0], dockManager),
+            render = new dockspawn.PanelContainer($("#render_window")[0], dockManager),
 
-        var editorDiv = $("#editor1_window")[0],
-            editor = new JSONEditor(editorDiv);
-
-        // Drag and drop support for the json editor.
-        editorDiv.addEventListener(
-          'dragover', 
-          function(evt){
-            evt.stopPropagation();
-            evt.preventDefault();
-            evt.dataTransfer.dropEffect = 'copy';
-          }, 
-          false
+            documentNode = dockManager.context.model.documentManagerNode,
+            statusNode = dockManager.dockDown(documentNode, status, 0.1),
+            renderNode = dockManager.dockFill(documentNode, render),
+            noiseNode = dockManager.dockLeft(renderNode, noise, 0.25),
+            editor1Node = dockManager.dockUp(noiseNode, editor1, 0.5);
+    }
         );
 
-        editorDiv.addEventListener(
-          'drop', 
-          function(evt){
-            evt.stopPropagation();
-            evt.preventDefault();
+requirejs(
+    ['jquery', 'cytoscape'],
 
-            var f = evt.dataTransfer.files[0],            
-                reader = new FileReader();
+    function($, cytoscape) {
+        console.log("Cytoscape loaded...");
 
-            console.log("Reading file: ", f);
+        window.cy = cytoscape({
+          container: $('#noise_window')[0],
 
-            reader.onload = function(e) {
-              console.log("Finished reading file: ", e);              
-
-              var t = e.currentTarget.result,
-                  j = JSON.parse(t);
-
-              editor.set(j);
-            };
-
-            reader.readAsText(f); 
-          }, 
-          false
-        );
-
-        editor.set({
-          'msg': 'Drag and drop a glow scene JSON file here.'
-        });
-
-        $('#noise_window').cytoscape({
-          style: cytoscape.stylesheet()
+          style: 
+            cytoscape.stylesheet()
             .selector('node')
               .css({
                 'content': 'data(name)',
@@ -138,42 +114,71 @@ requirejs(
           },
           
           layout: {
-            name: 'grid',
+            name: 'cose',
             padding: 10
           },
           
           ready: function(){
-            window.cy = this;
+            console.log("cytoscape ready...");
+//            window.cy = this;
+          }
             
-            // giddy up...
+        });     
+    }
+);
             
-            cy.elements().unselectify();
+requirejs(
+    ['jquery', 'jsoneditor'],
             
-            cy.on('tap', 'node', function(e){
-              var node = e.cyTarget; 
-              var neighborhood = node.neighborhood().add(node);
+    function($, JSONEditor) {
+        console.log("Editor loaded.");
               
-              cy.elements().addClass('faded');
-              neighborhood.removeClass('faded');
-            });
+        var editorDiv = $("#editor1_window")[0],
+            editor = new JSONEditor(editorDiv);
             
-            cy.on('tap', function(e){
-              if( e.cyTarget === cy ){
-                cy.elements().removeClass('faded');
-              }
+        // Drag and drop support for the json editor.
+        editorDiv.addEventListener(
+          'dragover', 
+          function(evt){
+            evt.stopPropagation();
+            evt.preventDefault();
+            evt.dataTransfer.dropEffect = 'copy';
+          }, 
+          false
+        );
+
+        editorDiv.addEventListener(
+          'drop', 
+          function(evt){
+            evt.stopPropagation();
+            evt.preventDefault();
+
+            var f = evt.dataTransfer.files[0],            
+                reader = new FileReader();
+
+            console.log("Reading file: ", f);
+
+            reader.onload = function(e) {
+              console.log("Finished reading file: ", e);              
+
+              var t = e.currentTarget.result,
+                  j = JSON.parse(t);
+
+              editor.set(j);
+            };
+
+            reader.readAsText(f); 
+          }, 
+          false
+        );
+
+        editor.set({
+          'msg': 'Drag and drop a glow scene JSON file here.'
             });
           }
-        });        
+);
 
-        var editor1 = new dockspawn.PanelContainer(editorDiv, dockManager),
-            noise = new dockspawn.PanelContainer($("#noise_window")[0], dockManager),
-            render = new dockspawn.PanelContainer($("#render_window")[0], dockManager);
 
-        var documentNode = dockManager.context.model.documentManagerNode,
-            statusNode = dockManager.dockDown(documentNode, status, 0.1),
-            renderNode = dockManager.dockFill(documentNode, render),
-            noiseNode = dockManager.dockLeft(renderNode, noise, 0.25),
-            editor1Node = dockManager.dockUp(noiseNode, editor1, 0.5);
     }
 );
 
