1c59f8c2ea02f07e7d28d921791855bbeea6d60c
Tim Finer <tfiner@fastmail.fm>
2014-09-27 10:22:45 -0700

    Revert "Failed experiment to add new items to a pane."
    This reverts commit 3f7dc91035ebab9d82599c3e164b233445a505fd.

--- ui/assets/js/main.js
+++ ui/assets/js/main.js
@@ -43,11 +43,9 @@ requirejs(
 
         var status = new dockspawn.PanelContainer($("#status_window")[0], dockManager);
 
-        var editorPaneDiv = $("#editor1_pane")[0], 
-            editorDiv = $("#editor1_window")[0],
+        var editorDiv = $("#editor1_window")[0],
             editor = new JSONEditor(editorDiv);
 
-        // TODO: get scene description
         editor.set({
           'array': [1, 2, 3],
           'boolean': true,
@@ -132,7 +130,7 @@ requirejs(
           }
         });        
 
-        var editor1 = new dockspawn.PanelContainer(editorPaneDiv, dockManager),
+        var editor1 = new dockspawn.PanelContainer(editorDiv, dockManager),
             noise = new dockspawn.PanelContainer($("#noise_window")[0], dockManager),
             render = new dockspawn.PanelContainer($("#render_window")[0], dockManager);
 
