4f5258c64fd42fda6ee735ba9f8136cc12311c75
Tim Finer <tfiner@fastmail.fm>
2014-10-18 16:26:01 -0700

    Serialization stuff and dead code pulled.

--- ui/assets/js/glowMain.js
+++ ui/assets/js/glowMain.js
@@ -41,45 +41,12 @@ define(
                 resize: {
                     enabled: true,
 
-                    start: function(e, ui, widget) {
-                        // console.log("START:\n", e, ui, widget);
-                    },
-
                     resize: function(e, ui, widget) {
-                        // console.log("RESIZE:\n", e, ui, widget);
                         events.resizing(widget);
-                    },
-
-                    stop: function(e, ui, widget) {
-                        var gridster    = $(".gridster ul").gridster().data('gridster'),
-                            glows       = $(".gridster li").find(".glow-window"),
-                            cells       = _.map(glows, function(g) { // Capture everything needed to restore a cell...
-                                            var p = $(g).parent(); 
-                                            return { 
-                                                id:     $(g).attr("id"), 
-                                                col:    $(p).attr("data-col"),
-                                                row:    $(p).attr("data-row"),
-                                                size_x: $(p).attr("data-sizex"),
-                                                size_y: $(p).attr("data-sizey")
-                                            }; 
-                                        }),
-                            js          = JSON.stringify(cells);
-
-                        console.log("cells: ", js);
-                        // console.log("STOP:\n", e, ui, widget);
-                        events.resizeStopped(widget);
-
-                        // Gridster erroneously assumes that none of the widgets will have moved.
-                        // Get all of the glow windows, get their names, then 
-                        //      from their parent cell obtain the position and size
-                        // serialize all three of these instead.
-                        // cells:  [{"id":"editor1_window","col":"1","row":"1","size_x":"2","size_y":"2"},{"id":"noise_window","col":"3","row":"1","size_x":"2","size_y":"2"},{"id":"tool_window","col":"5","row":"1","size_x":"3","size_y":"1"},{"id":"render_window","col":"8","row":"2","size_x":"12","size_y":"7"},{"id":"log_window","col":"8","row":"1","size_x":"3","size_y":"1"}] 
-
-                        localStorage.setItem('glowGrid', js);                        
                     }
                 }
             }).data("gridster"),
-            lastUserInputWasBackspace = false
+            lastUserInputWasBackspace = false;
 
         // This hackery prevents the retarded behavior of backspace 
         // meaning navigate back a page.
@@ -95,32 +62,6 @@ define(
                 return "Are you sure you want to leave this page?";
             }
         })
-
-        // Serialization not working!
-
-        // console.log("gridster:", gridster);
-        // console.log("grid:", grid);
-        // console.log("grid:", grid.serialize());
-
-        // Find the parent cell of the serialized id, and set the size appropriately.
-        gridSerialized = localStorage.getItem('glowGrid');
-        if (gridSerialized && gridSerialized.length) {
-            gridJson = JSON.parse(gridSerialized);
-            if (gridJson && gridJson.length) {
-                console.log("Serialized grid: ", gridJson);
-                _.each(gridJson, function(c){
-                    var cell = $("#" + c.id).parent();
-                    // gridster.resize_widget(cell, c.size_x, c.size_y);
-                    // gridster.mutate_widget_in_gridmap(cell, c.coords().grid, cell);
-                });
-            }
-            // gridJson = gridster.sort_by_row_and_col_asc(gridJson);
-
-
-            // $.each(serialization, function() {
-            //     grid.add_widget('<li />', this.size_x, this.size_y, this.col, this.row);
-            // });            
-        }
 
         // Start everything off in sync with gridster parent divs.
         console.log("Forcing a resize...");
