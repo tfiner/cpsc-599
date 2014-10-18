9a96ae656ca82eae6d036917bccaf22b6f4b6316
Tim Finer <tfiner@fastmail.fm>
2014-10-18 16:25:39 -0700

    Mostly fixed stupid focus bug.

--- ui/assets/js/glowEditor.js
+++ ui/assets/js/glowEditor.js
@@ -5,17 +5,29 @@
 // This file is the module for the JSON Editor.
 
 define(
-    ['jquery', 'jsoneditor'],
+    ['jquery', 'jsoneditor', 'gridster'],
 
     function($, JSONEditor) {
         console.log("editor loading...");
 
         var div = $("#editor1_window")[0],
             editor = new JSONEditor(div);
-
+      
         editor.set({
           'msg': 'Drag and drop a glow scene JSON file here.'
         });
+
+        $("#editor1_window").on("click", function(){
+                var gridster = $(".gridster ul").gridster().data('gridster');
+                gridster.disable().disable_resize();
+                console.log("click editor / disable grid resize");
+            });
+
+        $(".gridster li header").on("click", function(){
+                var gridster = $(".gridster ul").gridster().data('gridster');
+                gridster.enable().enable_resize();
+                console.log("click grid / enable grid resize");
+            });
 
         return {
             div:    div,
